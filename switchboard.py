108e3ea4a62b82330606889370588440cb20a9d5
James Bourbeau <jrbourbeau@gmail.com>
2019-07-14 09:27:48 -0500

    Add optional xarray deps

--- switchboard.py
+++ switchboard.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
-
 import os
-from texasbbq import (main,
-                      execute,
-                      conda_install,
-                      git_ls_remote_tags,
-                      IntegrationTestProject,
-                      setup_git,
-                      )
+
+from texasbbq import (
+    main,
+    execute,
+    conda_install,
+    git_ls_remote_tags,
+    IntegrationTestProject,
+    setup_git,
+)
 
 
-class DaskSource():
+class DaskSource:
 
     module = __name__
 
@@ -33,7 +33,7 @@ class DaskSource():
     def install(self, env):
         setup_git(self)
         execute("conda run -n {} pip install -e .".format(env))
-        os.chdir('../')
+        os.chdir("../")
 
 
 class XarrayTests(IntegrationTestProject):
@@ -51,14 +51,25 @@ class XarrayTests(IntegrationTestProject):
 
     @property
     def conda_dependencies(self):
-        return ["numpy pandas pytest"]
+        required = ["numpy", "pandas"]
+        optional = [
+            "scipy",
+            "zarr",
+            "netCDF4",
+            "bottleneck",
+            "cartopy",
+            "pynio",
+            "pseudonetcdf",
+        ]
+        testing = ["pytest"]
+        all_deps = required + optional + testing
+        return " ".join(all_deps)
 
     def install(self):
         execute("pip install -e .")
 
     def run_tests(self):
         execute("pytest xarray")
-
 
 
 if __name__ == "__main__":
